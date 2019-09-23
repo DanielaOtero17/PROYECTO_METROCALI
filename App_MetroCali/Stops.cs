a261a8944802d69e8108ccc6833c336f9c1bc310
1193476661 <1193476661@SC203E09>
2019-09-23 15:51:39 -0500

    STOPS IS ALREADY FINISHED.

--- App_MetroCali/Stops.cs
+++ App_MetroCali/Stops.cs
@@ -8,6 +8,25 @@ namespace App_MetroCali
 {
     class Stops
     {
+        private String STOPID { get; set; }
+        private String PLANVERSIONID { get; set; }
+        private String SHORTNAME { get; set; }
+        private String LONGNAME { get; set; }
+        private String GPS_X { get; set; }
+        private String GPS_y { get; set; }
+        private double DECIMALLONGITUD { get; set; }
+        private double DECIMALLATITUD { get; set; }
 
+        public Stops(string sTOPID, string pLANVERSIONID, string sHORTNAME, string lONGNAME, string gPS_X, string gPS_y, double dECIMALLONGITUD, double dECIMALLATITUD)
+        {
+            STOPID = sTOPID;
+            PLANVERSIONID = pLANVERSIONID;
+            SHORTNAME = sHORTNAME;
+            LONGNAME = lONGNAME;
+            GPS_X = gPS_X;
+            GPS_y = gPS_y;
+            DECIMALLONGITUD = dECIMALLONGITUD;
+            DECIMALLATITUD = dECIMALLATITUD;
+        }
     }
 }
