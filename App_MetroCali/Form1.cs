0d0cc3523c7d7f3d7360fd7d96da9c892eb8ca14
1193476661 <1193476661@SC203E09>
2019-09-23 15:59:54 -0500

    actualizing

--- App_MetroCali/Form1.cs
+++ App_MetroCali/Form1.cs
@@ -12,9 +12,33 @@ namespace App_MetroCali
 {
     public partial class Form1 : Form
     {
+
+        public List<Stops> Paradas = new List<Stops>();
+        public  List<Stops> ParadasEstaciones = new List<Stops>();
+        List<Stops> ParadasCalle = new List<Stops>();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void separarListasDeParadas()
+        {
+            for(int i = 0; i< Paradas.Count; i++)
+            {
+
+            }
+        }
+
+        public List<Stops> retornarLista()
+        {
+
+            return Paradas;
         }
     }
 }
